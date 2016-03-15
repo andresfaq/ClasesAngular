79ee5c59d77a98fff71330fe3eda32e4aa48f096
Andres <andres.artunduaga@correounivalle.edu.co>
2016-03-15 18:00:18 -0500

    sdf

--- Clase 2/app.js
+++ Clase 2/app.js
@@ -8,21 +8,87 @@ var app = angular.module('store', []);
 		this.products = gem;		
 		
 	});	
+
+	app.controller('PanelCtrl',function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+
+
+	app.controller('ReviewController',function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+		};
+
+
+	});
+
+
+
+	});
 	
 	var gem = [{	
 			name: 'Dodecahedron',
 			price: 2.95,
 			description: '. . .',
 			canPurchase: true,
-			soldOut: false, 
+			soldOut: false,
+			images: [
+						{
+							full: 'full.png',
+							thumb: 'thumb.png',
+						},
+						{
+							thumb: 'thumb.png',
+							full: 'full.png',							
+						}],
+			reviews: [{
+							stars: 5,
+							body: "I love this shit",
+							author:"joe@thomas.com"	
+						},
+						{
+							stars: 1,
+							body: "this product is something",
+							author: "hello@thomas.com"	
+						}]		
 			},
-			{	
+			/*{	
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: '. . .',
 			canPurchase: true,
-			soldOut: false, 
-			}];
+			soldOut: false,
+			images: [
+						{
+							full: 'full.png',
+							full: 'thumb.png',
+						}
+					],
+			reviews: [
+						{
+							stars: 5,
+							body: "I love this shit",
+							author:"joe@thomas.com"	
+						},
+						{
+							stars: 1,
+							body: "this product is something",
+							author: "hello@thomas.com"
+						}
+					 ]
+					 		  
+			}*/
+			]
+			
 	
 })();
 
